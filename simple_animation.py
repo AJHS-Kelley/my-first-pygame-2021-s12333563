1dfa9efeeb318a39100491d471af13f9a5158697
Joshua Clark <70532454+s12333563@users.noreply.github.com>
2022-01-12 12:58:57 -0500

    v0.4

--- simple_animation.py
+++ simple_animation.py
@@ -4,10 +4,10 @@ from PyGamePractice import GREEN
 import pygame, sys, time
 from pygame.locals import *
 
-# Setup PyGame
+# Setup PyGame.
 pygame.init()
 
-#Setup the Window
+#Setup the Window.
 WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
@@ -21,8 +21,14 @@ UPRIGHT = 'upright'
 
 MOVESPEED = 4
 
-# Setup Color Values
+# Setup Color Values.
 WHITE = (255, 255, 255)
 RED = (255, 0 , 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup the box data.
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3]
