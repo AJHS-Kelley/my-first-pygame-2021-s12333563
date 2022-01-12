6b3c8958d04b8b3372dd7b2ec18dcbaf7c231c05
Joshua Clark <70532454+s12333563@users.noreply.github.com>
2022-01-12 12:44:45 -0500

    Update simple_animation.py

--- simple_animation.py
+++ simple_animation.py
@@ -1,1 +1,21 @@
 # Simple Animation with PyGame, Joshua Clark, 1/12/22, 11:56AM, v0.0
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+
+#Setup the Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
+
+# Setup the direction variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
