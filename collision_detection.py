4e35b4fdff4c1546143079269b7388cfc2ef4686
Joshua Clark <70532454+s12333563@users.noreply.github.com>
2022-01-20 13:13:18 -0500

    Added FOODCOUNTER
    Filled in windowSurface.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Joshua Clark, 1/20/22, 11:40AM, v0.8
+# PyGame Collision Detection Practice, Joshua Clark, 1/20/22, 11:40AM, v0.9
 
 import pygame, sys, random
 from pygame.locals import *
@@ -77,5 +77,13 @@ while True:
       if event.type == MOUSEBUTTONUP:
           foods.append(pygame.Rect(event,pos[0], event.pos[1], FOODSIZE, FOODSIZE))      
         
+    foodCounter += 1
+    if foodCounter => NEWFOOD:
+        # Add new food.
+        foodCounter = 0
+        foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+
+    # Draw white background on Window Surface. 
+    windowSurface.fill(WHITE)    
 
     
